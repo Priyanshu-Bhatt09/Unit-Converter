06c0e05ad577c89859774c38359a71cba54989dd
Priyanshu-Bhatt09 <priyanshu.bhatt@nuvica.ai>
2026-04-04 09:28:35 +0530

    Implement unit conversion API with length, weight, and temperature support

--- backend/converter/views.py
+++ backend/converter/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here
+
+@api_view(['POST'])
+def convert_units(request):
+    value = float(request.data.get('value'))
+    from_unit = request.data.get('from_unit')
+    to_unit = request.data.get('to_unit')
+    category = request.data.get('category')
+
+    result = convert(value, from_unit, to_unit, category)
+
+    return Response({"result": result})
+
+def convert(value, from_unit, to_unit, category):
+    # length conversion to meters
+    length = {
+        "milimeter": 0.001,
+        "centimeter": 0.01,
+        "meter" : 1,
+        "Kilometer": 1000,
+        "inch": 0.0254,
+        "foot": 0.3048,
+        "yard": 0.9144,
+        "mile": 1609.34
+    }
+
+    #weight conversion to grams
+    weight = {
+        "milligram": 0.001,
+        "gram" : 1,
+        "kilogram": 1000,
+        "ounce": 28.3495,
+        "pound": 453.592
+    }
+
+    if category == "length":
+        return value * length[from_unit] / length[to_unit]
+    
+    elif category == "weight":
+        return value * weight[from_unit] / weight[to_unit]
+    
+    elif category == "temperature":
+        #convert to celcius first
+        if from_unit == "fahrenheit":
+            value = (value - 32) * 5/9
+        elif from_unit == "kelvin":
+            value = value - 273.15
+
+        #convert from celcius to target
+        if to_unit == "fahrenheit":
+            return value * 9/5 + 32
+        elif to_unit == "kelvin":
+            return value + 273.15
+        else:
+            return value
