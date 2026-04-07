1f000f5b55a695d3459fdd03beb1fff2119694df
Priyanshu-Bhatt09 <priyanshu.bhatt@nuvica.ai>
2026-04-07 22:53:43 +0530

    created the frontend and backend structure and logic

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Converter } from './converter/converter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Converter],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
 })
 export class App {
   protected readonly title = signal('frontend');
---
+++ frontend/src/app/converter/converter.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-converter',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './converter.html',
+  // styleUrl: './converter.css',
+})
+export class Converter {
+  value: number = 0;
+  category: string = 'length';
+  from_unit: string = '';
+  to_unit: string = '';
+  result: number = 0;
+
+units:any = {
+  length: ['millimeter','centimeter','meter','kilometer','inch','foot','yard','mile'],
+  weight: ['milligram','gram','kilogram','ounce','pound'],
+    temperature: ['celsius','fahrenheit','kelvin']
+};
+
+constructor(private http: HttpClient) {}
+
+convert() {
+  this.http.post<any>('http://127.0.0.1:8000/api/convert/', {
+    value: this.value,
+    from_unit: this.from_unit,
+    to_unit: this.to_unit,
+    category: this.category
+  }).subscribe(res => {
+    this.result = res.result;
+  });
+}
+}
